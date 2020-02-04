106d5d35c4d053fa54a372273811eabfd002797f
Jonah Petri <jonah@petri.us>
2020-02-04 09:45:44 -0500

    fix flaky test

--- tests/subscribe.rs
+++ tests/subscribe.rs
@@ -1,10 +1,10 @@
-/*
-TODO: fix flaky test
-
 use tmq::{subscribe, Result};
 use zmq::{Context, SocketType};
 
-use utils::{check_receive_multiparts, generate_tcp_address, sync_send_multipart_repeated};
+use std::time::Duration;
+use tokio::time::timeout;
+use futures::StreamExt;
+use utils::{generate_tcp_address};
 
 mod utils;
 
@@ -13,15 +13,19 @@ async fn receive_single_message() -> Result<()> {
     let address = generate_tcp_address();
     let ctx = Context::new();
     let topic: &[u8] = b"topic2";
-    let sock = subscribe(&ctx).connect(&address)?.subscribe(topic)?;
 
+    let mut sub_sock = subscribe(&ctx).connect(&address)?.subscribe(topic)?;
     let data = vec![topic, b"hello", b"world"];
+    let pub_sock = Context::new().socket(SocketType::PUB).unwrap();
+    pub_sock.bind(&address).unwrap();
 
-    // hack to send long enough for the subscriber to receive something
-    // TODO: bind instead of connect
-    sync_send_multipart_repeated(address, SocketType::PUB, data.clone(), 100000);
+    for _ in 0usize..5 {
+        pub_sock.send_multipart(&data, 0).unwrap();
+        if let Ok(Some(Ok(incoming))) = timeout(Duration::from_millis(100), sub_sock.next()).await {
+            assert_eq!(incoming, data.into_iter().map(tmq::Message::from).collect::<tmq::Multipart>());
+            return Ok(());
+        }
+    }
 
-    check_receive_multiparts(sock, vec![data]).await?;
-
-    Ok(())
-}*/
+    panic!("Didn't receive published message");
+}
