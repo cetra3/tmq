0e382a60b35033a52c99a6b06f9e93464abfefbc
rain2307 <xgz9725@proton.me>
2025-02-09 10:18:22 +0800

    Update example socket configuration and logging

--- examples/pull.rs
+++ examples/pull.rs
@@ -8,12 +8,13 @@ use tmq::{pull, Context, Result};
 #[tokio::main]
 async fn main() -> Result<()> {
     if let Err(_) = env::var("RUST_LOG") {
-        env::set_var("RUST_LOG", "subscribe=DEBUG");
+        env::set_var("RUST_LOG", "pull=DEBUG");
     }
 
     pretty_env_logger::init();
 
-    let mut socket = pull(&Context::new()).bind("tcp://127.0.0.1:7899")?;
+    let mut socket = pull(&Context::new())
+        .bind("tcp://127.0.0.1:7899")?;
 
     while let Some(msg) = socket.next().await {
         info!(
